2f496d4096a2b32f083e2d8ad921672109d0dab8
AndhikaPrasetya <andhikaprasetya542@gmail.com>
2024-02-09 09:31:38 +0700

    destination

--- assets/main.js
+++ assets/main.js
@@ -4,3 +4,22 @@ menuOpen.addEventListener('click',  function() {
    menuOpen.classList.toggle('bx-x')
    menuWrap.classList.toggle('active')
 })
+
+var swiper = new Swiper(".mySwiper", {
+   
+   grabCursor: true,
+   centeredSlides: false,
+   spaceBetween: 30,
+   slidesPerView: "auto",
+   coverflowEffect: {
+     rotate: 0,
+     stretch: 0,
+     depth: 0,
+     modifier: 1,
+     slideShadows: true,
+   },
+   navigation: {
+      nextEl: ".bxs-chevron-right-circle",
+      prevEl: ".bxs-chevron-left-circle",
+    }
+ });
